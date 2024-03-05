b068bd2f4002afdfae80aafc17232c683fc0d49a
wandleshen <10379215+wandleshen@users.noreply.github.com>
2024-03-05 14:04:56 +0800

    [Feature]Finish NURBS surface AABBs generation and reformat the codes

--- aabspline.py
+++ aabspline.py
@@ -1,31 +1,78 @@
 import torch
 
-def gen_aabb(knotvector_u, knotvector_v, ctrlpts, m, n, p, q, delta=1., eps=.01):
-    # Get all basic_funcs
+
+def gen_aabb(knotvector_u, knotvector_v, ctrlpts, m, n, p, q, delta=1.0, eps=0.01):
+    """
+    Generate Axis-Aligned Bounding Boxes (AABBs) for a given NURBS surface
+
+    Parameters:
+    knotvector_u: Knot vector in the U direction
+    knotvector_v: Knot vector in the V direction
+    ctrlpts: Control points
+    m, n: Number of AABBs in U or V direction
+    p, q: Degree of the surface in U or V direction
+    delta: Amount to expand the bounding box
+    eps: Value for the edge of surface's bounding boxes expansion
+
+    Returns:
+    aabb: Axis-Aligned Bounding Boxes
+    """
+
+    # Get all basic functions
     u, i = gen_intervals(knotvector_u, m, p, eps)
-    Ni = basic_func(u, p, i, knotvector_u) # [m, p+1, 3]
+    Ni = basic_func(u, p, i, knotvector_u)  # [m, p+1, 3]
     v, j = gen_intervals(knotvector_v, n, q, eps)
-    Nj = basic_func(v, q, j, knotvector_v) # [n, q+1, 3]
+    Nj = basic_func(v, q, j, knotvector_v)  # [n, q+1, 3]
 
-    # Cal `P({\hat u},{\hat v})`
-    # i: [m, p+1] j: [n, q+1]
-    i_expand = i[:, None, :, None].expand(-1, n, -1, q+1) # [m, n, p+1, q+1]
-    j_expand = j[None, :, None, :].expand(m, -1, p+1, -1) # [m, n, p+1, q+1]
-    Pij = ctrlpts[i_expand, j_expand] # [m, n, p+1, q+1, 3]
-    Ni_expand = Ni[:, None, :, None, :].expand(-1, n, -1, q+1, -1) # [m, n, p+1, q+1, 3]
-    Nj_expand = Nj[None, :, None, :, :].expand(m, -1, p+1, -1, -1) # [m, n, p+1, q+1, 3]
-    NiNj = aa_times(Ni_expand, Nj_expand) # [m, n, p+1, q+1, 3]
+    # Calculate `P({\hat u},{\hat v})`
+    i_expand = i[:, None, :, None].expand(-1, n, -1, q + 1)  # [m, n, p+1, q+1]
+    j_expand = j[None, :, None, :].expand(m, -1, p + 1, -1)  # [m, n, p+1, q+1]
+    Pij = ctrlpts[i_expand, j_expand]  # [m, n, p+1, q+1, 4]
+    Ni_expand = Ni[:, None, :, None, :].expand(
+        -1, n, -1, q + 1, -1
+    )  # [m, n, p+1, q+1, 3]
+    Nj_expand = Nj[None, :, None, :, :].expand(
+        m, -1, p + 1, -1, -1
+    )  # [m, n, p+1, q+1, 3]
+    NiNj = aa_times(Ni_expand, Nj_expand, True)  # [m, n, p+1, q+1, 3]
 
-    # N_i({\hat u})N_j({\hat v})P_{i,j}
-    NiNjP = torch.einsum('ijklm,ijkln->ijnm', NiNj, Pij) # [m, n, 3, 3]
+    # Calculate N_i({\hat u})N_j({\hat v})P_{i,j}
+    NiNjP = torch.einsum("ijklm,ijkln->ijnm", NiNj, Pij)  # [m, n, 4, 3]
+    NiNjP, weight = (
+        NiNjP[..., 0:3, :],
+        NiNjP[..., 3, :],
+    )  # Discard NURBS weight -> [m, n, 3, 3], [m, n, 3]
+    inverse_weight = torch.zeros_like(weight)
+    inverse_weight[..., 0] = 1.0 / weight[..., 0]
+    inverse_weight[..., 1] = -weight[..., 1] / (weight[..., 0] ** 2)
+    inverse_weight[..., 2] = -weight[..., 2] / (weight[..., 0] ** 2)
+    NiNjP = aa_times(
+        NiNjP, inverse_weight[:, :, None, :].expand(-1, -1, 3, -1), True
+    )  # [m, n, 3, 3]
+
+    # Calculate AABB
     aabb = torch.zeros([m, n, 2, 3])
     aabb[..., 0, :] = NiNjP[..., 0] - torch.abs(NiNjP[..., 1:3]).sum(dim=-1) * delta
     aabb[..., 1, :] = NiNjP[..., 0] + torch.abs(NiNjP[..., 1:3]).sum(dim=-1) * delta
-    return aabb.reshape(-1,2,3)
+    return aabb.reshape(-1, 2, 3)
 
-def gen_intervals(knotvector, n, p, eps=.01):
-    if n < len(knotvector)-2*p:
-        print('[ERROR] Too few sampled intervals for the given knotvector')
+
+def gen_intervals(knotvector, n, p, eps=0.01):
+    """
+    Generate intervals for a given knot vector.
+
+    Parameters:
+    knotvector: The knot vector
+    n: Number of intervals
+    p: Degree of the B-spline basis function
+    eps: Value for the edge of surface's bounding boxes expansion
+
+    Returns:
+    u: The affine arithmetics values for each interval
+    i: The index of the left knot of each interval
+    """
+    if n < len(knotvector) - 2 * p:
+        print("[ERROR] Too few sampled intervals for the given knotvector")
         raise Exception()
     # Generate intervals
     diff = knotvector[1:] - knotvector[:-1]
@@ -48,7 +95,7 @@ def gen_intervals(knotvector, n, p, eps=.01):
         splits = torch.repeat_interleave(splits, splits)
         intervals /= splits
     # Generate the cumulative intervals
-    cumsum = torch.zeros(intervals.shape[0]+1, dtype=torch.float32).cuda()
+    cumsum = torch.zeros(intervals.shape[0] + 1, dtype=torch.float32).cuda()
     cumsum[1:] = torch.cumsum(intervals, dim=0)
     cumsum[0] -= eps
     cumsum[-1] += eps
@@ -57,41 +104,81 @@ def gen_intervals(knotvector, n, p, eps=.01):
     range_t = torch.arange(-p, 1, 1).cuda()
     i = i[:, None] + range_t[None, :]
     u = torch.zeros_like(orig, dtype=torch.float32).cuda()
-    u[:, 0] = (orig[:, 1] + orig[:, 0]) / 2.
-    u[:, 1] = (orig[:, 1] - orig[:, 0]) / 2.
+    u[:, 0] = (orig[:, 1] + orig[:, 0]) / 2.0
+    u[:, 1] = (orig[:, 1] - orig[:, 0]) / 2.0
     return u, i
 
+
 def basic_func(u, p, i, knotvector):
+    """
+    Calculate the B-spline basis function N_{i,p}(u).
+
+    Parameters:
+    u: The affine arithmetics values
+    p: The degree of the B-spline basis function
+    i: The index of the left knot
+    knotvector: The knot vector
+
+    Returns:
+    N: The B-spline basis function values
+    """
     n = u.shape[0]
     m = i.shape[1]
     N = torch.zeros([n, m, 3], dtype=torch.float32).cuda()
     if p == 0:
-        mask = (knotvector[i] <= u[:, None, 0]) & (u[:, None, 0] < knotvector[i+1])
-        N[..., 0] = torch.where(mask, torch.ones_like(N[..., 0]), torch.zeros_like(N[..., 0]))
+        mask = (knotvector[i] <= u[:, None, 0]) & (u[:, None, 0] < knotvector[i + 1])
+        N[..., 0] = torch.where(
+            mask, torch.ones_like(N[..., 0]), torch.zeros_like(N[..., 0])
+        )
         return N
     a = torch.zeros([n, m, 3], dtype=torch.float32).cuda()
     b = torch.zeros([n, m, 3], dtype=torch.float32).cuda()
     # Cal the first part of N_{i,p}
-    divisor_ip = knotvector[i+p] - knotvector[i]
+    divisor_ip = knotvector[i + p] - knotvector[i]
     mask = torch.abs(divisor_ip) > 1e-6
-    a[..., 0] = torch.where(mask, (u[:, None, 0] - knotvector[i]), torch.zeros_like(a[..., 0]))
+    a[..., 0] = torch.where(
+        mask, (u[:, None, 0] - knotvector[i]), torch.zeros_like(a[..., 0])
+    )
     a[..., 1] = torch.where(mask, u[:, None, 1], torch.zeros_like(a[..., 1]))
-    N[mask] += aa_times(a[mask], basic_func(u, p-1, i, knotvector)[mask]) / divisor_ip[mask][...,None]
+    N[mask] += (
+        aa_times(a[mask], basic_func(u, p - 1, i, knotvector)[mask])
+        / divisor_ip[mask][..., None]
+    )
     # Cal the second part of N_{i,p}
-    divisor_ip1i1 = knotvector[i+p+1] - knotvector[i+1]
+    divisor_ip1i1 = knotvector[i + p + 1] - knotvector[i + 1]
     mask = torch.abs(divisor_ip1i1) > 1e-6
-    b[..., 0] = torch.where(mask, (knotvector[i+p+1] - u[:, None, 0]), torch.zeros_like(b[..., 0]))
+    b[..., 0] = torch.where(
+        mask, (knotvector[i + p + 1] - u[:, None, 0]), torch.zeros_like(b[..., 0])
+    )
     b[..., 1] = torch.where(mask, -u[:, None, 1], torch.zeros_like(b[..., 1]))
-    N[mask] += aa_times(b[mask], basic_func(u, p-1, i+1, knotvector)[mask]) / divisor_ip1i1[mask][...,None]
+    N[mask] += (
+        aa_times(b[mask], basic_func(u, p - 1, i + 1, knotvector)[mask])
+        / divisor_ip1i1[mask][..., None]
+    )
 
     return N
 
-def aa_times(lhs, rhs):
+
+def aa_times(lhs, rhs, is3x3=False):
+    """
+    Perform affine arithmetic operations.
+
+    Parameters:
+    lhs: Left-hand side affine form
+    rhs: Right-hand side affine form
+    is3x3: Flag to indicate if the operation is for values w/ 2 noises
+
+    Returns:
+    res: The result of the affine arithmetic operation
+    """
     res = torch.zeros_like(lhs).cuda()
-    abs_lhs = torch.abs(lhs)
-    abs_rhs = torch.abs(rhs)
+    # abs_lhs = torch.abs(lhs)
+    # abs_rhs = torch.abs(rhs)
     res[..., 0] = lhs[..., 0] * rhs[..., 0]
     res[..., 1] = lhs[..., 1] * rhs[..., 0] + lhs[..., 0] * rhs[..., 1]
     # res[..., 2] = (abs_lhs[..., 1] + abs_lhs[..., 2]) * (abs_rhs[..., 1] + abs_rhs[..., 2])
-    res[..., 2] = (lhs[..., 1] + lhs[..., 2]) * (rhs[..., 1] + rhs[..., 2])
+    if is3x3:
+        res[..., 2] = lhs[..., 2] * rhs[..., 0] + lhs[..., 0] * rhs[..., 2]
+    else:
+        res[..., 2] = (lhs[..., 1] + lhs[..., 2]) * (rhs[..., 1] + rhs[..., 2])
     return res
