d24e357623132a668ad75765d5f948b06f955eae
wandleshen <10379215+wandleshen@users.noreply.github.com>
2024-03-06 18:39:19 +0800

    [Feature]Refactor surface display backend to use GLFW for VTK window rendering and ImGui for display control

--- utils.py
+++ utils.py
@@ -1,37 +1,40 @@
+import imgui
+import glfw
+import torch
 import vtk
 import numpy as np
+from imgui.integrations.glfw import GlfwRenderer
 from geomdl import NURBS, knotvector
-import torch
 
 
 RES = 50
 
 
-def render(aabb1, aabb2, surf1, surf2, col1, col2):
-    # Create a renderer, render window, and interactor.
+def impl_glfw_init(window_name="SUI-NURBS", width=1280, height=720):
+    if not glfw.init():
+        print("Could not initialize OpenGL context")
+        exit(1)
+
+    # Create a windowed mode window and its OpenGL context
+    window = glfw.create_window(int(width), int(height), window_name, None, None)
+    if not window:
+        glfw.terminate()
+        print("Could not initialize Window")
+        exit(1)
+
+    glfw.make_context_current(window)
+
     renderer = vtk.vtkRenderer()
-    render_window = vtk.vtkRenderWindow()
+    render_window = vtk.vtkExternalOpenGLRenderWindow()
+    render_window.SetSize(width, height)
     render_window.AddRenderer(renderer)
-    render_window_interactor = vtk.vtkRenderWindowInteractor()
-    render_window_interactor.SetRenderWindow(render_window)
 
-    add_aabb(aabb1, renderer)
-    add_aabb(aabb2, renderer)
-    add_aabb(col1, renderer, isRed=False)
-    add_aabb(col2, renderer, isRed=False)
-
-    add_evalpts(surf1, renderer)
-    add_evalpts(surf2, renderer, isGreen=False)
-
-    # Set camera position, start rendering and interactor.
-    renderer.ResetCamera()
-    render_window.Render()
-    render_window_interactor.Initialize()
-    render_window_interactor.Start()
+    return window, render_window, renderer
 
 
-def add_aabb(pts, renderer, isRed=True):
+def draw_aabb(pts, renderer, isRed=True):
     # Iterate over all given AABB diagonal pairs, create and display each box.
+    actors = []
     for min_point, max_point in zip(pts[:, 0], pts[:, 1]):
         # Create AABB box.
         box = vtk.vtkCubeSource()
@@ -58,9 +61,11 @@ def add_aabb(pts, renderer, isRed=True):
 
         # Add to renderer.
         renderer.AddActor(actor)
+        actors.append(actor)
+    return actors
 
 
-def add_evalpts(surf, renderer, isGreen=True):
+def draw_surf(surf, renderer, isGreen=True):
     # Add evaluation points
     plane_points = np.array(surf.evalpts)
     points = vtk.vtkPoints()
@@ -90,6 +95,201 @@ def add_evalpts(surf, renderer, isGreen=True):
 
     # Add to renderer.
     renderer.AddActor(polydata_actor)
+    return [polydata_actor]
+
+
+def pan_camera(camera, right_amount, up_amount):
+    # Get the current position, focal point and view up vector of the camera
+    position = camera.GetPosition()
+    focal_point = camera.GetFocalPoint()
+    view_up = camera.GetViewUp()
+
+    # Convert tuples to numpy arrays
+    position = np.array(position)
+    focal_point = np.array(focal_point)
+    view_up = np.array(view_up)
+
+    # Compute the direction of projection or the view plane normal
+    view_plane_normal = position - focal_point
+    view_plane_normal = view_plane_normal / np.linalg.norm(view_plane_normal)
+
+    # Compute the right vector (cross product of view up vector and view plane normal)
+    right_vector = np.cross(view_up, view_plane_normal)
+    right_vector = right_vector / np.linalg.norm(right_vector)
+
+    # Compute the up vector (cross product of view plane normal and right vector, to ensure orthogonal)
+    up_vector = np.cross(view_plane_normal, right_vector)
+    up_vector = up_vector / np.linalg.norm(up_vector)
+
+    # Scale the pan movements to match the specified amounts
+    right_movement = right_vector * right_amount
+    up_movement = up_vector * up_amount
+
+    # Calculate the new camera position and focal point
+    new_position = position + right_movement + up_movement
+    new_focal_point = focal_point + right_movement + up_movement
+
+    # Update the camera's position and focal point
+    camera.SetPosition(new_position.tolist())
+    camera.SetFocalPoint(new_focal_point.tolist())
+    camera.SetViewUp(
+        view_up.tolist()
+    )  # Ensure to reset the view up vector too, if it has been changed
+
+
+def handle_changes(
+    index, key, draw_func, renderer, actors_dict, changes, checkboxes, *args, **kwargs
+):
+    if changes[index]:
+        if checkboxes[index]:
+            actors_dict[key] = draw_func(*args, renderer=renderer, **kwargs)
+        else:
+            for actor in actors_dict[key]:
+                renderer.RemoveActor(actor)
+
+
+def render(
+    aabb1,
+    aabb2,
+    surf1,
+    surf2,
+    extract1,
+    extract2,
+    window_name="SUI-NURBS",
+    width=1280,
+    height=720,
+):
+    window, render_window, renderer = impl_glfw_init(window_name, width, height)
+    imgui.create_context()
+    impl = GlfwRenderer(window)
+
+    # 创建VTK的交互器
+    interactor = vtk.vtkRenderWindowInteractor()
+    interactor.SetRenderWindow(render_window)
+    renderer.ResetCamera()
+    renderer.SetBackground(0.1, 0.2, 0.4)
+
+    lastX, lastY = width // 2, height // 2
+    windowSize = [width, height]
+
+    # 鼠标移动事件处理函数
+    def on_mouse_move(window, xpos, ypos):
+        global lastX, lastY
+        camera = renderer.GetActiveCamera()
+        if glfw.get_mouse_button(window, glfw.MOUSE_BUTTON_LEFT) == glfw.PRESS:
+            deltaX = xpos - lastX
+            deltaY = ypos - lastY
+            # 获取当前的相机
+            camera.Azimuth(-deltaX)
+            camera.Elevation(deltaY)
+        elif glfw.get_mouse_button(window, glfw.MOUSE_BUTTON_RIGHT) == glfw.PRESS:
+            deltaX = xpos - lastX
+            deltaY = ypos - lastY
+            # 在当前渲染窗口尺寸取一定比例的移动
+            deltaX /= windowSize[0]
+            deltaY /= windowSize[1]
+            # 水平和垂直平移视图
+            pan_camera(camera, -deltaX, deltaY)
+
+        camera.OrthogonalizeViewUp()
+        lastX = xpos
+        lastY = ypos
+
+    # 鼠标按钮事件处理函数
+    def on_mouse_button(window, button, action, mods):
+        global lastX, lastY
+        if button == glfw.MOUSE_BUTTON_LEFT:
+            if action == glfw.PRESS:
+                (xpos, ypos) = glfw.get_cursor_pos(window)
+                lastX = xpos
+                lastY = ypos
+
+    # 设置滚轮事件的回调函数
+    def on_mouse_scroll(window, xoffset, yoffset):
+        if yoffset > 0:
+            interactor.InvokeEvent("MouseWheelForwardEvent")
+        elif yoffset < 0:
+            interactor.InvokeEvent("MouseWheelBackwardEvent")
+
+    glfw.set_cursor_pos_callback(window, on_mouse_move)
+    glfw.set_scroll_callback(window, on_mouse_scroll)
+    glfw.set_mouse_button_callback(window, on_mouse_button)
+
+    checkboxes = [True, True, True, True]
+    changes = [False, False, False, False]
+    actors_dict = {}
+
+    actors_dict["aabb1"] = draw_aabb(aabb1, renderer)
+    actors_dict["aabb2"] = draw_aabb(aabb2, renderer)
+    actors_dict["ext1"] = draw_aabb(extract1, renderer, isRed=False)
+    actors_dict["ext2"] = draw_aabb(extract2, renderer, isRed=False)
+
+    actors_dict["surf1"] = draw_surf(surf1, renderer)
+    actors_dict["surf2"] = draw_surf(surf2, renderer, isGreen=False)
+
+    while not glfw.window_should_close(window):
+        impl.process_inputs()
+        imgui.new_frame()
+        imgui.begin("Dashboard", True)
+
+        changes[0], checkboxes[0] = imgui.checkbox("AABB1", checkboxes[0])
+        changes[1], checkboxes[1] = imgui.checkbox("AABB2", checkboxes[1])
+        changes[2], checkboxes[2] = imgui.checkbox("Surface1", checkboxes[2])
+        changes[3], checkboxes[3] = imgui.checkbox("Surface2", checkboxes[3])
+
+        imgui.end()
+        imgui.render()
+        render_window.Render()
+
+        handle_changes(
+            0, "aabb1", draw_aabb, renderer, actors_dict, changes, checkboxes, aabb1
+        )
+        handle_changes(
+            0,
+            "ext1",
+            draw_aabb,
+            renderer,
+            actors_dict,
+            changes,
+            checkboxes,
+            extract1,
+            isRed=False,
+        )
+        handle_changes(
+            1, "aabb2", draw_aabb, renderer, actors_dict, changes, checkboxes, aabb2
+        )
+        handle_changes(
+            1,
+            "ext2",
+            draw_aabb,
+            renderer,
+            actors_dict,
+            changes,
+            checkboxes,
+            extract2,
+            isRed=False,
+        )
+        handle_changes(
+            2, "surf1", draw_surf, renderer, actors_dict, changes, checkboxes, surf1
+        )
+        handle_changes(
+            3,
+            "surf2",
+            draw_surf,
+            renderer,
+            actors_dict,
+            changes,
+            checkboxes,
+            surf2,
+            isGreen=False,
+        )
+
+        impl.render(imgui.get_draw_data())
+        glfw.swap_buffers(window)
+        glfw.poll_events()
+
+    impl.shutdown()
+    glfw.terminate()
 
 
 def gen_surface(ctrlpts):
